ef11012538c1f8f6e5cd7ea1a2be27fbe773b52f
Christopher Manzano <christopher.manzano.vimos@gmail.com>
2023-05-01 17:05:34 +0200

    added dependencies

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""
+Install the package
+"""
+
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().split("\n")
+
+setup(
+    name='discord_clash_bot',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "discord_clash_bot = discord_clash_bot.cli.commands:cli"
+            ]
+        }
+)
+        
