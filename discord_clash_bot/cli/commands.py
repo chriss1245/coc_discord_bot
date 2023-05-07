ac5bfb519c09265d4a81d7c51b62c9def5e2a473
Christopher Manzano <christopher.manzano.vimos@gmail.com>
2023-05-07 22:57:57 +0200

    updated cli to support stop

--- discord_clash_bot/cli/commands.py
+++ discord_clash_bot/cli/commands.py
@@ -2,29 +2,70 @@
 cli commands
 """
 
+import asyncio
+import os
+import signal
 import click
-import toml
 
-from discord_clash_bot.bot.discord_bot import MyBot
-from discord_clash_bot.api.coc_wrapper import CocClient
-from discord_clash_bot.db.db import DBConnection
+from discord_clash_bot.utils.logging import get_logger
+from discord_clash_bot.utils.config import PROJECT_DIR
+
+logger = get_logger(__name__)
+pid_file = PROJECT_DIR / "discord_clash_bot.pid"
 
 @click.group()
 @click.pass_context
-@click.option("--secrets", "-s", type=click.Path(exists=True), required=True, default="secrets.toml")
-def cli(ctx, secrets):
+def cli(ctx):
+    """
+    Discord clash bot cli
+    """
     ctx.ensure_object(dict)
 
-    with open(secrets, "r", encoding="utf-8") as f:
-        secrets = toml.load(f)
-    ctx.obj["secrets"] = secrets
-
 @cli.command()
-@click.pass_context
-def run(ctx):
+def run():
     """
     Run the bot
     """
-    token = ctx.obj["secrets"]["discord"]["token"]
-    bot = MyBot(token)
-    bot.run()
+    from discord_clash_bot import main
+
+    if pid_file.exists():
+        logger.error("Bot already running")
+        return
+
+    with open(pid_file, "w", encoding="utf-8") as pid:
+        pid.write(str(os.getpid()))
+
+    try:
+        logger.info("Starting bot")
+        asyncio.run(main.run())
+    except KeyboardInterrupt:
+        logger.info("Stopping bot")
+        pid_file.unlink()
+
+@cli.command()
+def stop():
+    """
+    Stop the bot, if it is running. If the bot is not running, just delete the pid file.
+    """
+    if not pid_file.exists():
+        logger.error("Bot not running")
+        logger.debug(f"Not found {pid_file}, bot may be daemonized")
+        return
+
+    with open(pid_file, "r", encoding="utf-8") as pid:
+        pid = int(pid.read())
+    try:
+        os.kill(pid, signal.SIGTERM)
+    except ProcessLookupError:
+        logger.warning("Bot not running, deleting just pid record")
+
+    pid_file.unlink()
+    logger.info("Bot stopped")
+
+@cli.command()
+def restart():
+    """
+    Restart the bot
+    """
+    stop()
+    run()
