651e2065748c273f04224f691f640f290dac8359
Christopher Manzano <christopher.manzano.vimos@gmail.com>
2023-06-04 17:49:26 +0200

    dbugging ç

--- discord_clash_bot/utils/config.py
+++ discord_clash_bot/utils/config.py
@@ -9,7 +9,9 @@ PROJECT_DIR = Path(__file__).parent.parent.parent
 
 secrets_path = PROJECT_DIR / "secrets.toml"
 
+
 if not secrets_path.exists():
+    print(PROJECT_DIR.glob("*"))
     raise FileNotFoundError(f"No secrets.toml file found in {PROJECT_DIR}")
 
 
