24f736cf0f89af9550f8ecce17f1b42acf3a98f7
Christopher Manzano <christopher.manzano.vimos@gmail.com>
2023-06-04 17:54:51 +0200

    debugging

--- discord_clash_bot/utils/config.py
+++ discord_clash_bot/utils/config.py
@@ -4,15 +4,11 @@ Handles the configuration and secrets.
 from pathlib import Path
 import toml
 
-
 PROJECT_DIR = Path(__file__).parent.parent.parent
 
 secrets_path = PROJECT_DIR / "secrets.toml"
 
-
 if not secrets_path.exists():
-    print(list(PROJECT_DIR.glob("*")))
     raise FileNotFoundError(f"No secrets.toml file found in {PROJECT_DIR}")
-
 
 SECRETS = toml.load(secrets_path)
